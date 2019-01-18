fb928151e23a447657f1b35c417028140bc07df0
alsamzu <zulfalsam@gmail.com>
2019-01-18 16:05:50 +0300

    Screen to capture users name

--- app/src/main/java/com/example/zulfa/geoquiz/StoryActivity.java
+++ app/src/main/java/com/example/zulfa/geoquiz/StoryActivity.java
@@ -1,7 +1,9 @@
 package com.example.zulfa.geoquiz;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class StoryActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class StoryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_story);
+
+        Intent intent = getIntent();
+        String name = intent.getStringExtra(getString(R.string.key_name));
+
+        if (name ==null || name.isEmpty()){
+            name = "Friend";
+        }
+        Toast.makeText(this,name,Toast.LENGTH_LONG).show();
     }
 }
