801b6388f4ded6a8b7aa677335cadb99438f38fc
alsamzu <zulfalsam@gmail.com>
2019-01-22 12:40:26 +0300

    Displaying user input on a different activity

--- app/src/main/java/com/example/zulfa/geoquiz/StoryActivity.java
+++ app/src/main/java/com/example/zulfa/geoquiz/StoryActivity.java
@@ -3,21 +3,45 @@ package com.example.zulfa.geoquiz;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class StoryActivity extends AppCompatActivity {
+    private String name;
+    private TextView storyHeading;
+    private int textId;
+
+    public int getTextId() {
+        return textId;
+    }
+
+    public void setTextId(int textId) {
+        this.textId = textId;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_story);
 
+        storyHeading = (TextView) findViewById(R.id.genreTextView);
+
         Intent intent = getIntent();
-        String name = intent.getStringExtra(getString(R.string.key_name));
+         name = intent.getStringExtra(getString(R.string.key_name));
 
         if (name ==null || name.isEmpty()){
             name = "Friend";
         }
-        Toast.makeText(this,name,Toast.LENGTH_LONG).show();
+
+//        storyHeading.setText(name);
+        loadName();
+//        Toast.makeText(this,name,Toast.LENGTH_LONG).show();
+
+    }
+
+    private void loadName() {
+        String userName = getString(R.string.choose_your_adventure);
+        userName = String.format(userName,name);
+        storyHeading.setText(userName);
     }
 }
